d963da477118326d23c3227d5e266158aeb8a21a
Tianwei Zhao <51933284+ZTWHHH@users.noreply.github.com>
2020-09-24 12:24:21 -0600

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "functions_to_implement.cpp"
+#include <vector>
+
+std::vector<int> v{1,2,3,4};
+
+TEST_CASE ( "Sum are computed", "[sum]") {
+  REQUIRE( Sum(v) == 10 );
+}
+
+TEST_CASE ( "Product are computed", "[product]") {
+    REQUIRE( Sum(v) == 24 );
+}
+
+
