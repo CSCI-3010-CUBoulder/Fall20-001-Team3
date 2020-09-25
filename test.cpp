e04e9c5625849077815bcdae9c1ce90b81d4ca17
kusi3680 <45445645+kusi3680@users.noreply.github.com>
2020-09-24 22:48:34 -0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,18 +1,36 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "functions_to_implement.cpp"
 #include <vector>
 
-std::vector<int> v{1,2,3,4};
+/*
+int RemoveTwos(int original){
+	while (original % 2 != 1)
+    	{
+        	original = original % 2;
+    	}
 
-TEST_CASE ( "Sum are computed", "[sum]") {
-  REQUIRE( Sum(v) == 10 );
+}
+*/
+TEST_CASE("Remove Twos check", "[removed]")
+{
+    REQUIRE(RemoveTwos(7) == 7);
 }
 
-TEST_CASE ( "Product are computed", "[product]") {
-    REQUIRE( Product(v) == 24 );
+/*
+double Product(std::vector<double> nums)
+{
+    float product = 0;
+    for (unsigned int i = 0; i < nums.size(); i++)
+    {
+        product = product * i;
+    }
+    return product;
 }
+*/
 
-TEST_CASE ( "Twos are removed", "[product]") {
-    REQUIRE( RemoveTwos(7) == 7 );
+TEST_CASE("PRODUCT OF VECTOR", "[product]")
+{
+    std::vector<double> v{4.0, 1.0, 5.0, 6.0};
+    REQUIRE(Product(v) == 120.0);
 }
